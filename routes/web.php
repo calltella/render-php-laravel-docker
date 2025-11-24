cea896face72aa98ec2a839328b853afcde7c371
calltella <yuichi.asa@gmail.com>
2025-11-24 16:49:36 +0900

    token Fix.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/token', function (Request $request) {
+    $user = User::find(1);
+    $token = $user->createToken('test-token')->plainTextToken;
+
+    return response()->json([
+        'token' => $token
+    ]);
+});
+
