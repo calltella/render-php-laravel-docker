a6cfb65ed9916aa3541385aa9167af9190fc6a49
calltella <yuichi.asa@gmail.com>
2025-11-25 17:04:54 +0900

    api key auth

--- config/app.php
+++ config/app.php
@@ -99,6 +99,8 @@ return [
 
     'key' => env('APP_KEY'),
 
+    'x_api_key' => env('X_API_KEY',''),
+
     'previous_keys' => [
         ...array_filter(
             explode(',', (string) env('APP_PREVIOUS_KEYS', ''))
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,44 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/status', function () {
     return response()->json([
+        'server' => 'Render Server',
         'status' => 'ok',
         'timestamp' => now()->toDateTimeString()
     ]);
 });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/**
+ *  X-API-KEY、UserIDでトークンを取得
+ */
+Route::get('/token/{id}', function (Request $request, $id) {
+    // APIキーで認証（最低限のセキュリティ）
+    if ($request->header('X-API-KEY') !== config('app.x_api_key')) {
+        abort(403, 'Unauthorized');
+    }
+    \Log::info(config('app.x_api_key'));
+
+    // ユーザー取得
+    $user = User::findOrFail($id);
+
+    // スコープ付きトークン発行
+    $token = $user->createToken('custom-token', ['read', 'update'])->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'scopes' => ['read', 'update']
+    ]);
+});
+
+// token認証
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('v1')
+        ->group(function () {
+            Route::get('/user', function (Request $request) {
+                return $request->user();
+            });
+        });
+});
